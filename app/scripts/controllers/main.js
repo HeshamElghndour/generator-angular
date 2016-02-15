443963e836869fde74863968d5abb997d80df410
heshamElghndour <hesham.elghndour@gmail.com>
2016-02-15 17:02:05 +0200

    Creating the choosen image process

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,30 @@
  * Controller of the generatorAngularApp
  */
 angular.module('generatorAngularApp')
-  .controller('MainCtrl', function () {
+  .controller('MainCtrl', function ($scope,$mdMedia,$mdDialog ) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+    $scope.showAdvanced = function(ev) {
+      var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && $scope.customFullscreen;
+      $mdDialog.show({
+        templateUrl: 'views/contact-modal.html',
+        parent: angular.element(document.body),
+        targetEvent: ev,
+        clickOutsideToClose:true,
+        fullscreen: useFullScreen
+      })
+      .then(function(answer) {
+        $scope.status = 'You said the information was "' + answer + '".';
+      }, function() {
+        $scope.status = 'You cancelled the dialog.';
+      });
+      $scope.$watch(function() {
+        return $mdMedia('xs') || $mdMedia('sm');
+      }, function(wantsFullScreen) {
+        $scope.customFullscreen = (wantsFullScreen === true);
+      });
+    };
   })
